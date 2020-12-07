19a8c9f624097346eda693a9924ec8bee8ede396
unknown <elliottmcf@gmail.com>
2020-12-07 17:28:44 -0500

    rearranges .py file in preparation for scrape function

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
 import time
 from bs4 import BeautifulSoup as bs
@@ -12,29 +6,21 @@ from splinter import Browser
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-# In[2]:
-
-
 executable_path = {'executable_path': ChromeDriverManager().install()}
 browser = Browser('chrome', **executable_path, headless=False)
 
-
-# In[ ]:
-
-
 news_url = 'https://mars.nasa.gov/news/'
+jpl_url = 'https://www.jpl.nasa.gov'
+images_url = f'{jpl_url}/spaceimages/?search=&category=Mars'
+mars_facts_url = 'https://space-facts.com/mars/'
+astrogeo_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+
+
 browser.visit(news_url)
-
-
-# In[3]:
-
+time.sleep(0.2)
 
 news_html = browser.html
 news_soup = bs(news_html, 'html.parser')
-
-
-# In[4]:
-
 
 articles = news_soup.find_all('li', class_='slide')
 latest_article = articles[0]
@@ -42,25 +28,16 @@ latest_headline = latest_article.find('div', class_='content_title').text
 latest_teaser = latest_article.find('div', class_='article_teaser_body').text
 
 
-# In[5]:
-
-
-jpl_url = 'https://www.jpl.nasa.gov'
-images_url = f'{jpl_url}/spaceimages/?search=&category=Mars'
 browser.visit(images_url)
-
-
-# In[6]:
+time.sleep(0.1)
 
 
 full_image_button = browser.find_by_id('full_image')
 full_image_button.click()
 
+
 browser.links.find_by_partial_text('more info').click()
-
-
-# In[7]:
-
+time.sleep(0.1)
 
 featured_image_html = browser.html
 featured_image_soup = bs(featured_image_html,'html.parser')
@@ -69,14 +46,8 @@ featured_image_loc = featured_image_soup.find('figure', class_='lede')
 featured_image_url = f"{jpl_url}{featured_image_loc.find('a')['href']}"
 
 
-# In[8]:
-
-
-mars_facts_url = 'https://space-facts.com/mars/'
 browser.visit(mars_facts_url)
-
-
-# In[9]:
+time.sleep(0.1)
 
 
 mars_facts_table = pd.read_html(mars_facts_url, match='Recorded By')[0]
@@ -85,14 +56,8 @@ mars_facts_table.columns = ["Criteria", "Values"]
 mars_facts_html = mars_facts_table.to_html(index=False)
 
 
-# In[10]:
-
-
-astrogeo_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+time.sleep(0.1)
 browser.visit(astrogeo_url)
-
-
-# In[11]:
 
 
 hemisphere_image_urls = []
@@ -114,10 +79,7 @@ for x in range(4):
     hemisphere_image_urls.append(hemisphere_image)
 
     browser.back()
-    time.sleep(1)
-
-
-# In[12]:
+    time.sleep(0.2)
 
 
 browser.quit()
