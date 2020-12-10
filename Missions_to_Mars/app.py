4caca287396257b89e638d18eb830caefa03f385
unknown <elliottmcf@gmail.com>
2020-12-09 19:25:01 -0500

    Adds try-except for the case where no database is found

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -9,8 +9,25 @@ mongo = PyMongo(app)
 
 @app.route("/")
 def index():
-    mars_data = mongo.db.mars_data.find_one()
-    return render_template("index.html", mars_data=mars_data)
+    try:
+        mars_data = mongo.db.mars_data.find_one()    
+        return render_template("index.html", mars_data=mars_data)
+
+    except:
+        return """
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <div class="container" style="background: lightgray;">
+            <div class="row">
+                <div class="col-12">
+                    <div class="jumbotron text-center bg-dark text-light">
+                        <h1 class="display-4">Mission to Mars</h1>
+                        <br>
+                        <a class="btn btn-primary btn-lg" href="/scrape" role="button">Scrape new data</a>
+                    </div>
+                </div>
+            </div>
+        """
 
 
 @app.route("/scrape")
